ab6ef710179258d1f298851a8d40f5cf81f00748
Abhinav Anand <abhinavanand1905@gmail.com>
2017-03-29 10:02:40 +0530

    Add files via upload

---
+++ Drawing shapes/circle.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+
+pic = np.zeros((500,500,3), dtype = 'uint8')
+
+color=(255,0,255)
+cv2.circle(pic, (250, 250), 250, color)
+
+cv2.imshow('dark', pic)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
